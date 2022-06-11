567adbcd9caf7c83e03bb68cc861cfa89a474848
‘hepsontam’ <hepsontam@foxmail.com>
2022-06-12 04:14:16 +0800

    v3.0 增加签到后的刷新请求

--- BTBar.py
+++ BTBar.py
@@ -1,15 +1,23 @@
-#!/usr/bin/python3
+#################### 手动输入以下内容 ####################
+## 用户名 (手机号/邮箱)
+USER = ''
+
+## 密码
+PASSWORD = ''
+
+## 曾经登录的时间戳（若连续登录则取第一天）；
+## 无内容则提交当前登录时间；
+## 可自定义（在输出结果中提取，或自行抓取“Hm_lvt”字样的cookie）
+PastLoginTime = ''
+########################################################
+
 from requests import post, get
 from time import time
+from re import sub                     ## 正则替换
 from ast import literal_eval as le     ## String转化为List或Dictionary
 from urllib3 import disable_warnings
 from urllib3.exceptions import InsecureRequestWarning as w
 disable_warnings(w)                    ## 屏蔽无SSL验证的警告
-
-################ 手动输入以下内容 ##################
-USER = ''        ## 用户名 (手机号/邮箱)
-PASSWORD = ''    ## 密码
-#################################################
 
 HEADERS = {
 	"Accept": "*/*",
@@ -20,7 +28,8 @@ HEADERS = {
 }
 
 def btbar_login(BTBASESSID): 
-## 实际上可用旧cookie登录，但有效期太短（少于半天？），目前一律重新登录（传参'now'）
+## 实际上可用旧cookie登录，但有效期太短，目前一律重新登录（传参'now'）
+	if not USER: quit()
 	url = 'https://u.aibtba.com/user/login'
 	headers = {
 		"Content-Type": "application/x-www-form-urlencoded",
@@ -29,13 +38,11 @@ def btbar_login(BTBASESSID):
 		"Content-Length": "58"
 	}
 	headers.update(HEADERS)
-	
+
 	if BTBASESSID != 'now':
-		time_stamp = str(int(time()))
-		cookieText = 'BTBASESSID=' + BTBASESSID + '; Hm_lpvt_19a24d91cbe5649b8e7449e1b7c1a97e=' + time_stamp + '; Hm_lvt_19a24d91cbe5649b8e7449e1b7c1a97e=' + time_stamp
+		time_stamp, pastTime = get_time_stamp()
+		cookieText = 'BTBASESSID=' + BTBASESSID + '; Hm_lpvt_19a24d91cbe5649b8e7449e1b7c1a97e=' + time_stamp + '; Hm_lvt_19a24d91cbe5649b8e7449e1b7c1a97e=' + pastTime
 		headers['Cookie'] = cookieText
-		
-	if not USER: quit()
 		
 	data = {
 		'mobile_email': USER,
@@ -43,13 +50,14 @@ def btbar_login(BTBASESSID):
 		'submit': '1'
 	}
 	response = post(url=url, headers=headers, data=data, verify=False)
+	print('当前登录时间戳:', int(time()))
 	return response
 
 def btbar_checkin(cookie, coin):
 	url_0 = 'https://u.aibtba.com/sign?search_referer=https%3A%2F%2Fu.aibtba.com%2F&callback=Json'
 	url = 'https://u.aibtba.com/sign/select?callback=Json'
-	time_stamp = str(int(time()))
-	cookieText = cookie + 'Hm_lpvt_19a24d91cbe5649b8e7449e1b7c1a97e=' + time_stamp + '; Hm_lvt_19a24d91cbe5649b8e7449e1b7c1a97e='+ time_stamp + '; search_referer=https%253A%252F%252Fu.aibtba.com%252F'
+	time_stamp, pastTime = get_time_stamp()
+	cookieText = cookie + 'Hm_lpvt_19a24d91cbe5649b8e7449e1b7c1a97e=' + time_stamp + '; Hm_lvt_19a24d91cbe5649b8e7449e1b7c1a97e='+ pastTime + '; search_referer=https%253A%252F%252Fu.aibtba.com%252F'
 	## Hm_lpvt.. 当前时间戳; Hm_lvt.. 猜测是曾经登录时间（可多个，形如1654364167,1654364158）
 	headers_0 = {
 		"Referer": "https://www.aibtba.com/",
@@ -57,14 +65,38 @@ def btbar_checkin(cookie, coin):
 	}
 	headers_0.update(HEADERS)
 	response_0 = get(url=url_0, headers=headers_0, verify=False)  ## 点击签到
-	addCoin = le(response_0._content.decode('unicode_escape').replace('Json(', '').replace(')', ''))[1].replace('获得','').replace('BT币', '')
-	## 签到获取的BT币（当日首次签到可截取）：首先将返回信息从bytes形式解码为汉字，再列表化（literal_eval），列表的第二项为获取BT币的信息，最后去除无关文字，取其中数字
-	if addCoin == '已签到': addCoin = 0  ## 重复签到，无数字信息
+	addCoin = sub(r'获得|BT币', '', le(sub(r'Json\(|\)', '', response_0._content.decode('unicode_escape')))[1])
+	## 签到获取的BT币（当日首次签到可截取）：首先将返回信息从bytes形式解码为汉字，替换无关字符后列表化（literal_eval），列表的第二项为获取BT币的信息，最后去除无关文字，取其中数字
+	repeat = 0
+	if addCoin == '已签到': 
+		addCoin = 0  ## 重复签到，无数字信息
+		repeat = 1
 	coin += int(addCoin)
 	headers = headers_0
 	headers['Cookie'] = cookieText + '; bt_gold=' + str(coin) + '; bt_sign=%25E5%25B7%25B2%25E7%25AD%25BE%25E5%2588%25B0' ## bt_sign二次URL解码 -> 已签到
 	response = get(url=url, headers=headers, verify=False)  ## 获取签到信息
-	return response
+	return response, repeat, coin
+
+def refresh(cookieText, nowCoin):
+	url = 'https://www.aibtba.com'
+	headers = HEADERS
+	headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+	time_stamp, pastTime = get_time_stamp()
+	cookieText = sub(r'bt_gold=\d{1,4}', 'bt_gold=' + str(nowCoin), cookieText)
+	headers['Cookie'] = cookieText + '; bt_sign=%25E5%25B7%25B2%25E7%25AD%25BE%25E5%2588%25B0; search_referer=https%253A%252F%252Fu.aibtba.com%252F'
+	response = get(url=url, headers=headers, verify=False)
+	try: 
+		if response.status_code != 200:
+			print('\n', '刷新出错了（不影响币值）。')
+	except Exception as ex:
+		print(ex)
+
+def get_time_stamp():
+	time_stamp = str(int(time()))
+	pastTime = str(PastLoginTime)
+	if not pastTime.strip():
+		pastTime = time_stamp
+	return time_stamp, pastTime
 
 def check_status(code):
 	status = 'Error'
@@ -90,14 +122,18 @@ except Exception as ex:
 
 if not cookies: quit()
 
-checkinAction = btbar_checkin(cookies, coin)
+checkinAction, checkinJudge, coin = btbar_checkin(cookies, coin)
 # checkinResult = checkinAction.__dict__  ## 类成员遍历
 # for key in checkinResult:
 # 	print(key + ': ' + str(checkinResult[key]))
 
 try:
-	checkinResult = { 'status': checkinAction.status_code, 'result': le(checkinAction._content.decode('unicode_escape').replace('Json(', '').replace(')', '')) }
+	checkinResult = { 'status': checkinAction.status_code, 'result': le(sub('Json\(|\)', '', checkinAction._content.decode('unicode_escape'))) }
 	print('签到结果：', check_status(checkinResult['status']) + '，', end='')
+	getCoin = checkinResult['result']['count']
+	if checkinJudge: 
+		print('请勿重复签到，', end='')
+		getCoin -= 1
 except Exception as ex:
 	print(ex)
 	quit()
@@ -105,8 +141,10 @@ except Exception as ex:
 for key, value in checkinResult['result']['date'].items():
 	if value == '':
 		if key == '今天':
-			print('今日获得' + str(checkinResult['result']['count']) + 'BT币', '\n\n预计可得BT币：')
+			print('今日获得' + str(getCoin) + 'BT币', '\n\n预计可得BT币：')
 		continue
 	else:
 		print('  ' + key.replace('.', '月') + '日 -', value)
+
+refresh(cookies, coin)    ## 刷新
 
